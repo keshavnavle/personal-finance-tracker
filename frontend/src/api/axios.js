575744abc3d5902e9842364146a3ed33ea7167f3
keshavnavle <keshavnavale303@gmail.com>
2026-07-20 14:23:53 +0530

    Connect frontend to Render backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://personal-finance-tracker-backend-a7pd.onrender.com",
 });
 
 export default API;
